a4c44eacd11ad60c9113c338bcaff1e4ab3babb5
melvinlamar <melomuriithi@gmail.com>
2019-10-09 12:32:01 +0300

    add function for add pizza button

--- JS/script.js
+++ JS/script.js
@@ -27,3 +27,19 @@ $("table").hide();
     this.total = total;
     this.orderNo = orderNo;
   }
+
+  $('.btn.add-pizza').click(function() {
+    var sizeOfPizza = $(".size option:selected").val();
+    var toppingsOfPizza = $(".toppings option:selected").val();
+    var crustOfPizza = $(".crust option:selected").val();
+    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+    order = order + 1;
+    grandTotal = grandTotal + total;
+
+
+    var newPizza = new Pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
+
+    var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+
+    $("#pizza").append(newRow);
+  });
